714d8e581833341e1a4459500f41e5665f53d4a1
stamenione <vidrak97@gmail.com>
2025-03-18 19:55:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpLearning
+{
+    internal class Program
+    {
+        public string? Salary { get; private set; } = "Not Enough";
+
+        static void Main(string[] args)
+        {
+            string requirements, miracleMax = "Hello World";
+            requirements = miracleMax = "It would take a miracle.";
+            Console.WriteLine(requirements);
+            Console.WriteLine(miracleMax);
+
+            miracleMax = "Hello World!";
+            Program program = new();
+            program = new Program();
+        }
+    }
+}
